001cf2244931f531831a7a4197714b114cf69525
osbornroxas <osbornroxas02@gmail.com>
2020-08-15 04:08:43 -0700

    chapter 2

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,20 @@
 DROP DATABASE IF EXISTS just_tech_news_db;
-
 CREATE DATABASE just_tech_news_db;
+
+CREATE SCHEMA users;
+
+
+
+
+INSERT INTO users (username, email, password)
+VALUES (
+    "Lernantino",
+    "lernantino@gmail.com",
+    "password1234"
+  );
+  
+UPDATE users
+SET username = "Lernantino",
+  email = "lernantino@gmail.com",
+  password = "newPassword1234"
+WHERE id = 1;
