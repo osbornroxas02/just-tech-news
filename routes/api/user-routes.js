e3ba511917361e7c34d7e760aad12b23938d84c8
osbornroxas <osbornroxas02@gmail.com>
2020-08-16 03:12:21 -0700

    post-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,13 +3,6 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-    // User.findAll()
-    //     .then(dbUserData => res.json(dbUserData))
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.status(500).json(err);
-    //     });
     User.findAll({
         attributes: { exclude: ['password'] }
     })
@@ -22,22 +15,6 @@ router.get('/', (req, res) => {
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
-    // User.findOne({
-    //     where: {
-    //         id: req.params.id
-    //     }
-    // })
-    //     .then(dbUserData => {
-    //         if (!dbUserData) {
-    //             res.status(404).json({ message: 'No user found with this id' });
-    //             return;
-    //         }
-    //         res.json(dbUserData);
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.status(500).json(err);
-    //     });
     User.findOne({
         attributes: { exclude: ['password'] },
         where: {
