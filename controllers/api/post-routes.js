a93eff1fdce76c0908a3dfd607b8cfa91eaf7a94
osbornroxas <osbornroxas02@gmail.com>
2020-08-19 22:42:05 -0700

    handlebar install

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const { Post, User } = require('../../models');
+
 const { Post, User, Vote, Comment } = require("../../models");
 
 const sequelize = require('../../config/connection');
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
+//Handlebars const
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+//Handlebars app
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to database and server
 sequelize.sync({ force: false }).then(() => {
@@ -17,3 +27,6 @@ sequelize.sync({ force: false }).then(() => {
 });
 
 //when updating the relationships between the tables, use sequelize.sync({ force: true }) to drop the tables and recreate them! Once confirmed the database tables were recreated, switch back to using { force: false }
+
+
+
